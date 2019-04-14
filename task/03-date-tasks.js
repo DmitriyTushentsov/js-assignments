03e489e7e9dff921baf3e9fc7457ca02aa37d790
Dmitriy <fr80@list.ru>
2019-04-14 20:43:19 +0300

    Feat: new tasks added

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -94,7 +94,8 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    let a = Math.abs((date.getUTCHours() * 60 + date.getUTCMinutes() - date.getUTCMinutes() * 12) / 2) % 360;
+    return (a <= 180 ? a : 360 - a) / 180 * Math.PI;
 }
 
 
--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -128,7 +128,7 @@ function isTriangle(a,b,c) {
  *  
  */
 function doRectanglesOverlap(rect1, rect2) {
-    throw new Error('Not implemented');
+    return rect1.left < rect2.left + rect2.width && rect1.left + rect1.width > rect2.left && rect1.top < rect2.top + rect2.height && rect2.top + rect2.height > rect2.height;
 }
 
 
